f238cea602de444d380b71ff5ecf952f18480d4a
marosak <68249226+marosak@users.noreply.github.com>
2020-08-13 18:20:53 +0200

    Update and rename README.md to přihlášení do github cz.py
    tohle je první program na mim githubu ve kterém jde o to že budete vyplňovat to co to chce

---
+++ přihlášení do github cz.py
@@ -0,0 +1,12 @@
+# program 1
+
+import time
+
+print("Ahoj přihlas se do githubu")
+time.sleep(3)
+a = input("napiš jméno: ")
+print("dobře")
+b = input("napiš heslo: ")
+time.sleep(3)
+print("výtej v githubu")
+time.sleep(5)
